854858f16049cf76795bae990e3be6f1b9dcda1f
Avraj19 <avrajsbw@gmail.com>
2020-05-28 10:19:21 +0100

    def hello

--- general_run.py
+++ general_run.py
@@ -1,4 +1,4 @@
-from general_test_file import formated_name
+from general_test_file import *
 
 
 # test setup
@@ -7,3 +7,9 @@ expected_output = 'Avraj'
 
 f_name = formated_name('    avraj   ')
 print(formated_name(known_input) == expected_output)
+
+# say_hello test
+hello_known_input = ' hello  '
+hello_known_output = 'Hello'
+
+print(formated_hello(hello_known_input) == hello_known_output)
--- general_test_file.py
+++ general_test_file.py
@@ -1,2 +1,5 @@
 def formated_name(name):
     return name.title().strip()
+
+def formated_hello(name):
+    return name.title().strip()
